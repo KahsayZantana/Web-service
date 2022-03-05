1b193b8a7a42650126db1198dc92acf76ea1de01
Kahsay Zantana <kahsayzantana038@gmail.com>
2022-03-04 22:54:14 -0500

    Adds repl.it configuration

--- index.php
+++ index.php
@@ -2,15 +2,13 @@
   <head>
     <tittle>PHP File!</tittle>
     </head>
-<body>
-   <h1>PHP File!</h1>
-    
-   <p>This is HTML!</p>
-
-   <?php
-
-   echo "<p>This is PHP!</P>";
+  <body>
+    <h1>PHP File!</h1>
+    <p>This is HTML!</p>
+    <?php
+     echo "<p>This is PHP!</P>";
    ?>
-</body>
+   <p>This is HTML</P>
 
+</body>
 </html>
