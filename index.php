b5436a656e4963a7404a2a43fbe60ffdb6b72101
Zantana Kahsay <91621786+KahsayZantana@users.noreply.github.com>
2022-03-05 04:08:36 +0000

    Adds text to index.php frpm.it

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
 <html>
+  
   <head>
+    
     <tittle>PHP File!</tittle>
     </head>
   <body>
     <h1>PHP File!</h1>
     <p>This is HTML!</p>
     <?php
+
      echo "<p>This is PHP!</P>";
+
    ?>
+
    <p>This is HTML</P>
 
+    <p>Hello from repl.it!</p>
 </body>
 </html>
