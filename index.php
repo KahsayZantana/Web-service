4326d4b0bd99d8151b79d352679a018b0cd18dd5
Zantana Kahsay <91621786+KahsayZantana@users.noreply.github.com>
2022-03-05 04:40:34 +0000

    changed index.html to index.php

--- index.php
+++ index.php
@@ -1,20 +1,47 @@
 <html>
-  
-  <head>
+<head>
+<title>Bond Web Service Demo</title>
+<style>
+body {font-family:georgia;}
+</style>
+<script src="https://code.jquery.com/jquery-latest.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+$(document).ready(function() {  
+
+	$('.category').click(function(e){
+        e.preventDefault(); //stop default action of the link
+		cat = $(this).attr("href");  //get category from URL
+		loadAJAX(cat);  //load AJAX and parse JSON file
+	});
+});	
+
+
+function loadAJAX(cat)
+{
+	//AJAX connection will go here
+    alert('cat is: ' + cat);
+}
     
-    <tittle>PHP File!</tittle>
-    </head>
-  <body>
-    <h1>PHP File!</h1>
-    <p>This is HTML!</p>
-    <?php
+function toConsole(data)
+{//return data to console for JSON examination
+	console.log(data); //to view,use Chrome console, ctrl + shift + j
+}
 
-     echo "<p>This is PHP!</P>";
+function bondJSON(data){
+//JSON processing data goes here
+}
 
-   ?>
-
-   <p>This is HTML</P>
-
-    <p>Hello from repl.it!</p>
-</body>
+</script>
+</head>
+	<body>
+	<h1>Bond Web Service</h1>
+		<a href="year" class="category">Bond Films By Year</a><br />
+		<a href="box" class="category">Bond Films By International Box Office Totals</a>
+		<h3 id="filmtitle">Title Will Go Here</h3>
+		<div id="films">
+			<p>Films will go here</p>
+		</div>
+		<div id="output">Results go here</div>
+	</body>
 </html>
