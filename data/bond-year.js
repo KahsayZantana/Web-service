47541494bab05ba2bfc50d0cf7dfb0ea2a31e0e9
Zantana Kahsay <91621786+KahsayZantana@users.noreply.github.com>
2022-03-05 20:12:31 +0000

    Code

--- data/bond-year.js
+++ data/bond-year.js
@@ -1,113 +1,113 @@
 {
-  "title": "Bond Films by Year",
-	"films":[
-			  {
+    "title": "Bond Films by Year",
+    "films":[
+              {
 				"Film":1,
-				"Title":"Empire",
-				"Year":2014,
-				"Director":"Lee Daniels",
-				"Producers":"Lee Daniels and Danny Strong",
-				"Writers":"Lee Daniels and Danny Strong",
-				"Composer": "Fil Eisler,Rodney Jerkins, Ester Dean, Jim Beanz",
-				"Budget":"$3,000,000.00 per episode",
-				"BoxOffice":"$303,841M(United States)",
-				"Image":"for-your-eyes-only.jpg"
-			  },
-			  {
-				"Film":2,
-				"Title":"Titanic",
-				"Year":1997,
-				"Director":"James Cameron",
-				"Producers":"James Cameron, Jon Landau, Pamela Easley",
-				"Writers":"James Cameron",
-				"Composer":"John Barry",
-				"Bond":"Pierce Brosnan ",
-				"Budget":"$2,000,000.00",
-				"BoxOffice":"$2.202 billion",
-				"Image":"a-view-to-a-kill.jpg"
-			  },
-			  {
-				"Film":3,
-				"Title":"beats",
-				"Year":2019,
-				"Director":"Chris Robinson",
-				"Producers":"",
-				"Writers":"Richard Maibaum and Paul Dehn",
-				"Composer":"John Barry",
-				"Bond":"Sean Connery",
-				"Budget":"> £25,000",
-				"BoxOffice":"$325,493",
-				"Image":"goldfinger.jpg"
-			  },
-			  {
+                "Title":"Empire",
+                "Year":2014,
+                "Director":"Lee Daniels",
+                "Producers":"Lee Daniels and Danny Strong",
+                "Writers":"Lee Daniels and Danny Strong",
+                "Composer": "Fil Eisler,Rodney Jerkins, Ester Dean, Jim Beanz",
+                "Budget":"$3,000,000.00 per episode",
+                "BoxOffice":"$303,841M(United States)",
+                "Image":"for-your-eyes-only.jpg"
+              },
+              {
+                "Film":2,
+                "Title":"Titanic",
+                "Year":1997,
+                "Director":"James Cameron",
+                "Producers":"James Cameron, Jon Landau, Pamela Easley",
+                "Writers":"James Cameron",
+                "Composer":"John Barry",
+                "Bond":"Pierce Brosnan ",
+                "Budget":"$2,000,000.00",
+                "BoxOffice":"$2.202 billion",
+                "Image":"a-view-to-a-kill.jpg"
+              },
+              {
+                "Film":3,
+                "Title":"beats",
+                "Year":2019,
+                "Director":"Chris Robinson",
+                "Producers":"",
+                "Writers":"Richard Maibaum and Paul Dehn",
+                "Composer":"John Barry",
+                "Bond":"Sean Connery",
+                "Budget":"> £25,000",
+                "BoxOffice":"$325,493",
+                "Image":"goldfinger.jpg"
+              },
+              {
 				"Film":4,
-				"Title":"Ginny & Georgia",
-				"Year":2021,
-				"Director":"Anya Adams",
-				"Producers":"Debra J. Fisher",
-				"Writers":"Sarah Lampert, Debra J.Fisher, Mike Gauyo, Ali Laventhol",
-				"Image":"thunderball.jpg"
-			  },
-			  {
-				"Film":5,
-				"Title":"Five feet apart",
-				"Year":2019,
-				"Director":"Justin Baldoni",
-				"Producers":"Justin Baldoni, Christopher H.Warner, Cathy Schulman",
-				"Writers":"Rachael Lippincott,Tobias Iaconis,Mikki Daughtry ",
-				"Composer":"Brian Theodore Tyler,Breton Vivian ",
-				"Budget":"$7,000,000.00",
-				"Image":"you-only-live-twice.jpg"
-			  },
-			  {
-				"Film":6,
-				"Title":"Don't mess with an angel",
-				"Year":2008,
-				"Director":"Víctor Manuel Fouilloux",
-				"Producers":"Nathalie Lartilleux",
-				"Writers":"Carlos Romero",
-				"Composer":"Jorge Eduardo Murguia",
-				"Image":"on-her-majestys-secret-service.jpg"
-			  },
-			  {
+                "Title":"Ginny & Georgia",
+                "Year":2021,
+                "Director":"Anya Adams",
+                "Producers":"Debra J. Fisher",
+                "Writers":"Sarah Lampert, Debra J.Fisher, Mike Gauyo, Ali Laventhol",
+                "Image":"thunderball.jpg"
+              },
+              {
+                "Film":5,
+                "Title":"Five feet apart",
+                "Year":2019,
+                "Director":"Justin Baldoni",
+                "Producers":"Justin Baldoni, Christopher H.Warner, Cathy Schulman",
+                "Writers":"Rachael Lippincott,Tobias Iaconis,Mikki Daughtry ",
+                "Composer":"Brian Theodore Tyler,Breton Vivian ",
+                "Budget":"$7,000,000.00",
+                "Image":"you-only-live-twice.jpg"
+              },
+              {
+                "Film":6,
+                "Title":"Don't mess with an angel",
+                "Year":2008,
+                "Director":"Víctor Manuel Fouilloux",
+                "Producers":"Nathalie Lartilleux",
+                "Writers":"Carlos Romero",
+                "Composer":"Jorge Eduardo Murguia",
+                "Image":"on-her-majestys-secret-service.jpg"
+              },
+              {
 				"Film":7,
-				"Title":"Jane the Virgin",
-				"Year":2014,
-				"Producers":"Jennie Snyder Urman, Perla Farias",
-				"Director":" Melanie Mayron",
-				"Image":"diamonds-are-forever.jpg"
-			  },
-			  {
-				"Film":8,
-				"Title":"Resort to love",
-				"Year":2021,
-				"Director":"Steven Tsuchida",
-				"Producers":"Alicia Augello Cook,Jeremy Kipp Walker and Maggie Malina ",
-				"Writers":"Tabi McCartney and Dana Schmalenberg",
-				"Composer":"Laura Anne Karpman and Atif Afzal",
-				"Budget":"over $10 million USD",
-				"Image":"live-and-let-die.jpg"
-			  },
-			  {
+                "Title":"Jane the Virgin",
+                "Year":2014,
+                "Producers":"Jennie Snyder Urman, Perla Farias",
+                "Director":" Melanie Mayron",
+                "Image":"diamonds-are-forever.jpg"
+              },
+              {
+                "Film":8,
+                "Title":"Resort to love",
+                "Year":2021,
+                "Director":"Steven Tsuchida",
+                "Producers":"Alicia Augello Cook,Jeremy Kipp Walker and Maggie Malina ",
+                "Writers":"Tabi McCartney and Dana Schmalenberg",
+                "Composer":"Laura Anne Karpman and Atif Afzal",
+                "Budget":"over $10 million USD",
+                "Image":"live-and-let-die.jpg"
+              },
+              {
 				"Film":9,
-				"Title":"Game of Thrones",
-				"Year":2011,
-				"Director":"Alan Taylor,Mark Mylod,Alexander John Graves,Jeremy Podeswa",
-				"Composer":"composer Ramin Djawadi",
-				"Budget":"over $10 million per episode",
-				"Image":"the-man-with-the-golden-gun.jpg"
-			  },
-			  {
-				"Film":10,
-				"Title":"Squid Game",
-				"Year":2021,
-				"Director":"Hwang Dong-hyuk",
-				"Producers":"Hwang Dong-hyuk",
-				"Writers":"Hwang Dong-hyuk",
-				"Composer":"Jung Jae-il",
-				"Budget":"$3.5 million.",
-				"Image":"the-spy-who-loved-me.jpg"
-			  },
-			 
-    ]
+                "Title":"Game of Thrones",
+                "Year":2011,
+                "Director":"Alan Taylor,Mark Mylod,Alexander John Graves,Jeremy Podeswa",
+                "Composer":"composer Ramin Djawadi",
+                "Budget":"over $10 million per episode",
+                "Image":"the-man-with-the-golden-gun.jpg"
+              },
+             
+              {
+                "Film":10,
+                "Title":"Squid Game",
+                "Year":2021,
+                "Director":"Hwang Dong-hyuk",
+                "Producers":"Hwang Dong-hyuk",
+                "Writers":"Hwang Dong-hyuk",
+                "Composer":"Jung Jae-il",
+                "Budget":"$3.5 million.",
+                "Image":"the-spy-who-loved-me.jpg"
+              }
+            ]
 }
